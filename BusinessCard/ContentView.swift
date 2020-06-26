8799288a42c4c2d1d672465a663552984e9d021d
Tejasree V <tejasree.vangapelli@gmail.com>
2020-06-26 13:48:42 -0700

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by tejasree vangapalli on 6/26/20.
+//  Copyright © 2020 tejasree vangapalli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
